17811099cf91dd806c713358a19666ce691a6f1a
Luan Oliveira <lpablo611@gmail.com>
2022-01-07 22:26:39 -0400

    South America

--- pages/index.js
+++ pages/index.js
@@ -58,13 +58,13 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Gráficos</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Charts
+        <h1 className="text-6xl font-bold mb-2">
+          População da América Latina
         </h1>
         <div>
           <Chart
@@ -75,6 +75,7 @@ export default function Home() {
             options={options}
           />
         </div>
+        <p>Dados IBGE 2020</p>
       </main>
     </div>
   )
